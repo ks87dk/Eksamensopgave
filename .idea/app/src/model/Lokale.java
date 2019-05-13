b6c3aa35fb6bd3b415bb7eb2cd42779089ef300a
Burhan Öztürk <burh0125@edu.easj.dk>
2019-05-13 12:24:32 +0200

    Redigeret Lokale.java

--- .idea/app/src/model/Lokale.java
+++ .idea/app/src/model/Lokale.java
@@ -1,4 +1,23 @@
-public class Lokale implements LokaleImpl{
+public class Lokale implements LokaleImpl
+{
+    public String getLokaleNavn() {
+        return lokaleNavn;
+    }
+
+    public void setLokaleNavn(String lokaleNavn) {
+        this.lokaleNavn = lokaleNavn;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    private String lokaleNavn;
+    private String location;
 
 
 }
--- .idea/app/src/model/LokaleImpl.java
+++ .idea/app/src/model/LokaleImpl.java
@@ -1,5 +1,5 @@
 public interface LokaleImpl{
 
 
-    
+
 }
