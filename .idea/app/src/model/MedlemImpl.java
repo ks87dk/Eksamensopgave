d9b2417c03b4b8d68220aea1f4eac4fd895bd85e
Burhan Öztürk <burh0125@edu.easj.dk>
2019-05-13 12:18:20 +0200

    Redigeret MedlemdImpl.java

--- .idea/app/src/model/MedlemImpl.java
+++ .idea/app/src/model/MedlemImpl.java
@@ -3,5 +3,5 @@ public interface MedlemImpl
     void opretMedlemmer(String virksomhedsnavn,int cvr);
     void opdatereMedlemmer(String virksomhedsnavn,int cvr);
     void sletMedlemmer(int cvr);
-    
+
 }
